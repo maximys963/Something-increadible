82111235ab69883104035775fc4a47708e0879ea
maximys963 <maxim.p.jsdev@gmail.com>
2019-04-11 22:01:55 +0300

    bolerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import './App.css';
+import MainScreen from './containers/main screen/main-screen';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <MainScreen/>
+            </div>
+        );
+    }
+}
+
+export default App;
